b3d2bdd4137d5f29179e882682a93e1d52e743be
Victor Seager <vseager@gmail.com>
2015-08-06 16:53:15 +0100

    Update Type.php

--- app/code/local/Edge/Block/Block/Adminhtml/Type.php
+++ app/code/local/Edge/Block/Block/Adminhtml/Type.php
@@ -6,7 +6,7 @@ class Edge_Block_Block_Adminhtml_Type extends Mage_Adminhtml_Block_Widget_Grid_C
     {
         $this->_controller = 'adminhtml_type';
         $this->_blockGroup = 'block';
-        $this->_headerText = Mage::helper('block')->__('Block');
+        $this->_headerText = Mage::helper('block')->__('Manage Block Types');
         $this->_addButtonLabel = Mage::helper('block')->__('Add Block Type');
         parent::__construct();
     }
