a8fe5ac925b8883e392c069549fe076044324f93
Victor Seager <vseager@gmail.com>
2015-08-06 16:52:59 +0100

    Update Block.php

--- app/code/local/Edge/Block/Block/Adminhtml/Block.php
+++ app/code/local/Edge/Block/Block/Adminhtml/Block.php
@@ -6,7 +6,7 @@ class Edge_Block_Block_Adminhtml_Block extends Mage_Adminhtml_Block_Widget_Grid_
     {
         $this->_controller = 'adminhtml_block';
         $this->_blockGroup = 'block';
-        $this->_headerText = Mage::helper('block')->__('Block');
+        $this->_headerText = Mage::helper('block')->__('Manage Blocks');
         $this->_addButtonLabel = Mage::helper('block')->__('Add Block');
         parent::__construct();
     }
